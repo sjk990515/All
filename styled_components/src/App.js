ae54058b07a25d9760ea445bf4eda6386f159688
prdg <prdg990515@gmail.com>
2022-12-20 18:16:05 +0900

    redux + -

--- styled_components/src/App.js
+++ styled_components/src/App.js
@@ -1,38 +1,31 @@
-import logo from "./logo.svg";
+// src/App.js
+
 import React from "react";
-import styled from "styled-components";
-import "./App.css";
+import { useSelector, useDispatch } from "react-redux"; // import 해주세요.
+import { minusOne, plusOne } from "./redux/modules/counter";
 
-const StBox = styled.div`
-  width: 100px;
-  height: 100px;
-  border: 1px solid ${(props) => props.borderColor};
-  margin: 20px;
-`;
-const boxList = ["red", "green", "blue"];
-
-// 색을 넣으면, 이름을 반환해주는 함수를 만듭니다.
-const getBoxName = (color) => {
-  switch (color) {
-    case "red":
-      return "빨간 박스";
-    case "green":
-      return "초록 박스";
-    case "blue":
-      return "파란 박스";
-    default:
-      return "검정 박스";
-  }
-};
-
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+  const number = useSelector((state) => state.counter.number);
   return (
     <div>
-      {boxList.map((item) => {
-        return <StBox borderColor={item}>{getBoxName(item)}</StBox>;
-      })}
+      <p>{number}</p>
+      <button
+        onClick={() => {
+          dispatch(plusOne());
+        }}
+      >
+        +1
+      </button>
+      <button
+        onClick={() => {
+          dispatch(minusOne());
+        }}
+      >
+        -1
+      </button>
     </div>
   );
-}
+};
 
 export default App;
